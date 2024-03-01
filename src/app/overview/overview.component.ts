c00495575c9264010518750f4ab5d26b2fdb12e3
VannWasHere <ateen1203@gmail.com>
2024-03-01 11:39:01 +0700

    Successful Adding

--- src/app/overview/overview.component.ts
+++ src/app/overview/overview.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { CustomerService } from '../services/data.service';
 @Component({
   selector: 'app-overview',
   templateUrl: './overview.component.html',
   styleUrl: './overview.component.css'
 })
-export class OverviewComponent {
-
+export class OverviewComponent implements OnInit {
+  customerList: any[] = [];
+  constructor(private customerService: CustomerService){}
+  // OnInit => When the component rendered this func will run 
+  ngOnInit() {
+    this.customerList = this.customerService.getData()
+  }
 }
--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -5,12 +5,47 @@ import { BehaviorSubject } from "rxjs";
     providedIn: 'root'
 })
 export class CustomerService{
-    private customer_list = new BehaviorSubject<any>(null);
+    private customer_list = new BehaviorSubject<any[]>([
+        {
+            'firstName': 'John',
+            'lastName': 'Doe',
+            'jobTitle': 'Software Engineer',
+            'dob': '05 Aug 1990',
+            'salary': 120000,
+            'address': '123 Main St',
+            'gender': 'Male',
+            'isMarried': true
+        },
+        {
+            'firstName': 'Jane',
+            'lastName': 'Smith',
+            'jobTitle': 'UX Designer',
+            'dob': '20 Jan 1985',
+            'salary': 95000,
+            'address': '456 Oak Ave',
+            'gender': 'Female',
+            'isMarried': false
+        },
+        {
+            'firstName': 'Michael',
+            'lastName': 'Johnson',
+            'jobTitle': 'Network Administrator',
+            'dob': '15 Sep 1995',
+            'salary': 80000,
+            'address': '789 Pine Blvd',
+            'gender': 'Male',
+            'isMarried': true
+        }    
+]);
+    
     customerList$ = this.customer_list.asObservable();
-
     addData = (data: object) => {
         const current_list = this.customer_list.value;
-        const updated_list = {...current_list, data}
+        const updated_list = [...current_list, data]
         this.customer_list.next(updated_list);
+    }
+
+    getData = () => {
+        return this.customer_list.value
     }
 }
